dbc427574fb74ed78ed23fe9eb2feb711b57b129
YichengShen <jsszsyc@hotmail.com>
2021-04-06 14:22:13 -0400

    improve the layout of Home on desktops

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { StoreContext } from "contexts/StoreContext";
 import css from "./Home.module.css";
 
 import Post from "./Post.js";
+import { Grid } from "@material-ui/core";
 
 function Home() {
   let {
@@ -22,25 +23,31 @@ function Home() {
   let { postId } = useParams(); // the variable name has to match the parameter name
 
   return (
-    <div className={css.container}>
-      {posts
-        .filter((post) =>
-          typeof postId !== "undefined" ? post.id === postId : true
-        )
-        .sort((a, b) => new Date(b.datetime) - new Date(a.datetime))
-        .map((post) => (
-          <Post
-            key={post.id}
-            user={findUser(post, users)}
-            post={post}
-            comments={findComments(post, comments)}
-            likes={findLikes(post, likes, currentUserId)}
-            onLike={addLike}
-            onUnlike={removeLike}
-            onComment={addComment}
-          />
-        ))}
-    </div>
+    <Grid container direction="row">
+      <Grid item xs={false} sm={3} md={4} xl={5} />
+      <Grid item xs={12} sm={6} md={4} xl={2}>
+        <div className={css.container}>
+          {posts
+            .filter((post) =>
+              typeof postId !== "undefined" ? post.id === postId : true
+            )
+            .sort((a, b) => new Date(b.datetime) - new Date(a.datetime))
+            .map((post) => (
+              <Post
+                key={post.id}
+                user={findUser(post, users)}
+                post={post}
+                comments={findComments(post, comments)}
+                likes={findLikes(post, likes, currentUserId)}
+                onLike={addLike}
+                onUnlike={removeLike}
+                onComment={addComment}
+              />
+            ))}
+        </div>
+      </Grid>
+      <Grid item xs={false} sm={3} md={4} xl={5} />
+    </Grid>
   );
 }
 
